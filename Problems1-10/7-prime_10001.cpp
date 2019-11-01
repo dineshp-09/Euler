d3643e3cb3a19d486fc6878bc00499b8bfb8b6e3
Dinesh Patil <44475250+dineshp-09@users.noreply.github.com>
2019-11-01 15:12:02 +0800

    Update 7-prime_10001.cpp

--- Problems1-10/7-prime_10001.cpp
+++ Problems1-10/7-prime_10001.cpp
@@ -1,23 +1,28 @@
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+
 #include<iostream>
-#include<vector>
 
 using namespace std;
 
-void PrimeNumber(long int num) {
-    vector<long int> prime(1, 2);
-    for (long int i = 2; prime.size() <= num; i++) {
-        bool isPrime = true;
-        for (long int j = 2; j < i; j++) {
+void PrimeNumber(int num) {
+    int count = 1;
+    //even numbers are omitted because all the prime numbers are odd
+    for (int i = 3; count < num; i+=2) {
+        for (int j = 3; j <= i; j+=2) {
             if(i % j == 0) {
-                isPrime = false;
+                if (i == j) {
+                    count++;
+                    if (count == num)
+                        cout<<i<<endl;
+                }
                 break;
             }
         }
-        if (isPrime) {
-            prime.push_back(i);
-        }
     }
-    cout<<prime[num]<<endl;
 }
 
 int main() {
